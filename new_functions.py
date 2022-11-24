82d9f3e497adee92f233394201d608f7750e928c
Rahul-Meow-Meow <118549036+Rahul-Meow-Meow@users.noreply.github.com>
2022-11-24 18:59:52 +0530

    Update new_functions.py

--- new_functions.py
+++ new_functions.py
@@ -34,4 +34,37 @@ def insert_hospital(table):
             con.commit()
     except Exception:
         print("Database not found")
+#blood group
+
+def split(s):
+    if len(s)==2:#corresponding to O+, O-, A+, A-, B+(yeah be positive ppl),B-
+        gr=s[0:1]
+        rh=s[1:]
+        return gr.upper(),rh
+    elif len(s)==3:#corresponds to AB+ and AB-
+        gr=s[0:2]
+        rh=s[2:]
+        return gr,rh
+def match(gr,rh):
+    if rh=="+":
+        if gr in ["A","B"]:
+            x=["O+","O-",gr+"+",gr+"-"]
+        elif gr in "AB":
+            x=["O+","O-","A+","A-","B+","B-","AB+","AB-"]
+    elif rh=="-":
+        if gr in ["A","B"]:
+            x=["O-",gr+"-"]
+        elif gr=="AB":
+            x=["O-","A-","B-","AB-"]
+        elif gr=='O':
+            x=['O-']
+    return x
+#Only for testing purposes
+#gr,rh=split("A+")
+#print(match(gr,rh))
+#n=input('Enter your blood group')
+#gr,rh=split(n)
+#print(match(gr,rh))
+
+#Looks legit, will be helpful. - Bharat.
 #insert_hospital('employee')
