347580cc98742b78d4303766ff4efd8dee3455da
BharatK49 <117009094+BharatK49@users.noreply.github.com>
2022-11-27 22:26:58 +0530

    Update new_functions.py

--- new_functions.py
+++ new_functions.py
@@ -1,13 +1,15 @@
+#Edit 1: Commented the create function as it's pretty useless for the time being.
+#Edit 2: Commented Insert_hospital function as it has been already coded within login() function.
 import mysql.connector
 con=mysql.connector.connect(host='localhost',user='root',passwd='root',database='hello')
 cur=con.cursor()
-def create(table):
-    try:
-        query="create table {}(Name varchar(30),Age int(4), Contact_details int(23))".format(table)
-        cur.execute(query)
-        print("Table created succesfully")
-    except Exception:
-        print("Database already exists")
+#def create(table):
+#    try:
+#        query="create table {}(Name varchar(30),Age int(4), Contact_details int(23))".format(table)
+#        cur.execute(query)
+#        print("Table created succesfully")
+#    except Exception:
+#        print("Database already exists")
 #create('Gautham')
 def fetch(table):
     try:
@@ -21,17 +23,17 @@ def fetch(table):
 #print(display('employee'))
 #create('Hello2')
 #print(fetch('employee'))
-def insert_hospital(table):
-    n=int(input("Enter number of donors:"))
-    try:
-        for i in range(0,n):
-            sno=int(input("Enter serial number"))
-            name=input("Enter name:")
-            age=input("Enter age:")
-            c_no=int(input("Enter Phone number of person:"))
-            query='insert into {} values("{}","{}",{},{})'.format(table,sno,name,age,c_no)
-            cur.execute(query)
-            con.commit()
-    except Exception:
-        print("Database not found")
+#def insert_hospital(table):
+#    n=int(input("Enter number of donors:"))
+#    try:
+#        for i in range(0,n):
+#            sno=int(input("Enter serial number"))
+#            name=input("Enter name:")
+#            age=input("Enter age:")
+#            c_no=int(input("Enter Phone number of person:"))
+#            query='insert into {} values("{}","{}",{},{})'.format(table,sno,name,age,c_no)
+#            cur.execute(query)
+#            con.commit()
+#    except Exception:
+#        print("Database not found")
 #insert_hospital('employee')
