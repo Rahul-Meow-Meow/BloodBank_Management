7bc212c39743ee30a6759ff699f3e4ee67a75081
BharatK49 <117009094+BharatK49@users.noreply.github.com>
2022-11-29 23:14:36 +0530

    Update new_functions.py

--- new_functions.py
+++ new_functions.py
@@ -1,15 +1,28 @@
 #Edit 1: Commented the create function as it's pretty useless for the time being.
 #Edit 2: Commented Insert_hospital function as it has been already coded within login() function.
+#Edit 3: removed Insert_Hospital Function as it's useless now.
+#Edit 4: Renamed file from new_functions to Sql_functions (tbh it's only 1 function but we'll change that later asfdsgfsdf).
 import mysql.connector
-con=mysql.connector.connect(host='localhost',user='root',passwd='root',database='hello')
+
+##def create_database():
+##    try:
+##        database = input("Enter database name:")
+##        query = "create database {}".format(database)
+##        cur.execute(query)
+##        print("Database created succesfully!")
+##        return database
+##    except Exception:
+##        print("Database already exists.")
+##a = create_database()
+con=mysql.connector.connect(host='localhost',user='root',passwd='Dharmodynamics', database = "bloodbank_management")
 cur=con.cursor()
-#def create(table):
-#    try:
-#        query="create table {}(Name varchar(30),Age int(4), Contact_details int(23))".format(table)
-#        cur.execute(query)
-#        print("Table created succesfully")
-#    except Exception:
-#        print("Database already exists")
+##def create(table):
+##    try:
+##        query="create table {}(Name varchar(30),Age int(4), Contact_details int(23))".format(table)
+##        cur.execute(query)
+##        print("Table created succesfully")
+##    except Exception:
+##        print("Database already exists")
 #create('Gautham')
 def fetch(table):
     try:
@@ -19,21 +32,3 @@ def fetch(table):
         return r
     except Exception:
         print("Database doesnt exist")
-
-#print(display('employee'))
-#create('Hello2')
-#print(fetch('employee'))
-#def insert_hospital(table):
-#    n=int(input("Enter number of donors:"))
-#    try:
-#        for i in range(0,n):
-#            sno=int(input("Enter serial number"))
-#            name=input("Enter name:")
-#            age=input("Enter age:")
-#            c_no=int(input("Enter Phone number of person:"))
-#            query='insert into {} values("{}","{}",{},{})'.format(table,sno,name,age,c_no)
-#            cur.execute(query)
-#            con.commit()
-#    except Exception:
-#        print("Database not found")
-#insert_hospital('employee')
