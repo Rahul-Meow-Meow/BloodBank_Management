a4957fe7f0ff45013e88b8152a60561f4738e714
BharatK49 <117009094+BharatK49@users.noreply.github.com>
2022-12-07 22:54:17 +0530

    Update Donor_code.py

--- Donor_code.py
+++ Donor_code.py
@@ -1,11 +1,13 @@
 # Donor code
 
+import time
 import mysql.connector
-con=mysql.connector.connect(host='localhost',user='root',passwd='root',database='bloodbank_management',autocommit=True)
+import json
+con=mysql.connector.connect(host='localhost',user='root',passwd='Dharmodynamics',database='bloodbank_management',autocommit=True)
 cur=con.cursor()
 
 try:
-    Query ="create table {}(Full_Name varchar(50),Age int(3),Blood_Group varchar(4),Sex varchar(15),Contact_Number int(11),Address varchar(200),Amount_of_blood_donated_in_mL float(6),Date_of_donation date default(curdate()))".format('bloodbank')
+    Query ="create table {}(Full_Name varchar(50),Age int(3),Blood_Group varchar(4),Sex varchar(15),Contact_Number bigint,Address varchar(200),Amount_of_blood_donated_in_mL float(6),Date_of_donation date default(curdate()))".format('bloodbank')
     cur.execute(Query)
     con.commit()
 except Exception:
@@ -16,27 +18,33 @@ def bloodbank_login():
     while tries>0:
         pwd=input("Enter password for login:")
         if pwd == "Dharmodynamics":
-            print("Successfully logged into the Bloodbank database:")
+            time.sleep(1.25)
+            print("Successfully logged into the Bloodbank database!\n\n")
             ch = ''
             while ch != 'n':
                 print("Please select an option below:")
-                print("a. Insert donor data.")
-                print("b. Show list of donors.")
-                ch = input("Enter an option:\nPress n to logout of the bloodbank.").lower()
+                print("a. Insert donor data.\n")
+                print("b. Show list of donors.\n")
+                ch = input("Enter an option.\nPress n to logout of the bloodbank:\n").lower()
                 if ch == 'a':
                     bloodbank_input()
                 elif ch == 'b':
                     show_don_list()
                 elif ch == 'n':
-                    print("Logging out...")
-                    print("You have logged out.")
+                    print("Logging out...\n")
+                    time.sleep(1.25)
+                    print("You have logged out.\nRedirecting you to the main menu.\n\n")
+                    time.sleep(1)
                     break
             break
         elif pwd!='Dharmodynamics':
-            print("Incorrect password")
+            time.sleep(0.75)
+            print("Incorrect password!! Try again.")
             tries-=1
         if tries==0:
-            print("Im tired of your crap")
+            time.sleep(1.75)
+            print("Too many login failures. Please try again sometime.\nRedirecting you to the main menu\n\n")
+            time.sleep(1)
             break
 
 
@@ -55,8 +63,10 @@ def bloodbank_input():
         query='insert into {} values("{}",{},"{}","{}",{},"{}",{},{})'.format('bloodbank',f_name,age,bl_gr,Sex,c_no,address,req,date)
         cur.execute(query)
         con.commit()
+        time.sleep(1.5)
         ch=input("Do you want to continue?(y/n):").lower()
         if ch=='y':
+            time.sleep(1)
             count+=1
             continue
         else:
@@ -66,7 +76,10 @@ def show_don_list():
     query = "select * from {}".format('bloodbank')
     cur.execute(query)
     s = cur.fetchall()
+    print("List of Donors:")
     for j in s:
-        print(j)
+        d = dict(zip(["Full Name", "Age","Blood Group", "Sex", "Contact Number", "Address", "Amount of blood donated in mL","Date_of_donation"], j))
+        time.sleep(1)
+        print(json.dumps(d, indent = 4, default = str))
 
 
