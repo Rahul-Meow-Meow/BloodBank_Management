d6530ca5b2062ff2bf4ceeed0624a3caa8674afc
BharatK49 <117009094+BharatK49@users.noreply.github.com>
2022-12-20 05:57:15 +0530

    Update _main_.py

--- _main_.py
+++ _main_.py
@@ -1,12 +1,12 @@
 # Main:
 
 import mysql.connector
-con = mysql.connector.connect(host='localhost', user='root',
-                              passwd='root', autocommit=True)
-cur = con.cursor()
+con=mysql.connector.connect(host='localhost',user='root',
+                            passwd='Dharmodynamics',autocommit=True)
+cur=con.cursor()
 
 try:
-    query = 'create database {}'.format('bloodbank_management')
+    query = "create database {}".format('bloodbank_management')
     cur.execute(query)
     con.commit()
 except Exception:
@@ -18,27 +18,28 @@ from Donor_code import *
 from Recipient_code import *
 import time
 
+
+
+
+
 ch = ''
 
-while ch not in ('e', 'E'):
+while ch not in ('e','E'):
     time.sleep(1)
-    print ('Welcome to our Bloodbank management system!\n')
-    print ('1. Register Hospital.\n')
-    print ('2. Login to Hospital.\n')
-    print ('3. Login to Bloodbank.\n')
-    ch = \
-        input('''Please select one of the above options to proceed.
-                 \nPress 'E' to exit.''')
+    print("Welcome to our Bloodbank management system!\n")
+    print("1. Register Hospital.\n")
+    print("2. Login to Hospital.\n")
+    print("3. Login to Bloodbank.\n")
+    ch = input('''Please select one of the above options to proceed.
+               \nPress 'E' to exit:''')
     if ch == '1':
         register()
     elif ch == '2':
-        Hosp_ID = int(input('Enter Hospital ID:'))
+        Hosp_ID = int(input("Enter Hospital ID:"))
         check(Hosp_ID)
     elif ch == '3':
         bloodbank_login()
-    elif ch in ('e', 'E'):
+    elif ch in ('e','E'):
         break
     else:
         continue
-
-        
